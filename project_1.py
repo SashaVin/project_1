e34e396253d4167cfc8c58461db908b62a1119d2
SashaVin <55402145+SashaVin@users.noreply.github.com>
2020-03-14 13:06:21 +0700

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -2,10 +2,10 @@ import random as r
 import time as t
 
 def bindiv(lst, number, len_):
-    if lst.count(number) == 0:
+    if len_ == 1 and lst[0] != number:
         return None
     if len_ == 1:
-        return 0
+        return 1
     if len_ % 2 == 1:
         if lst[(len_ // 2 + 1):].count(number) > 0:
             return len_ // 2 + 1 + bindiv(lst[(len_ // 2 + 1):], number, len(lst[(len_ // 2 + 1):]))
@@ -25,16 +25,17 @@ lst = []
 for i in range(a1, a2):
     lst.append(i)
 
+
 t2 = t.perf_counter()
 
 print(bindiv(lst, number, len(lst)))
 
 t3 = t.perf_counter()
 
-f = 0
+f = 1
 while len(lst) > 1:
     if lst.count(number) == 0:
-        print(None)
+        f = None
         break
     if len(lst) % 2 == 1:
         if lst[(len(lst) // 2 + 1):].count(number) > 0:
@@ -49,8 +50,7 @@ while len(lst) > 1:
         else:
             lst = lst[:(len(lst) // 2)]
 t4 = t.perf_counter()
-if f != 0:
-    print(f)
+print(f)
 print('Время выполнения рекурсивной функции:', ':{:.8f}'. format(t3 - t2))
 print('Время выполнения цикла:', ':{:.8f}'. format(t4 - t3))
 t5 = t.perf_counter()
